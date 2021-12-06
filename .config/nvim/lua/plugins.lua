5905760bacee3243f21790c4d6a991bd1b24ec86
Filip Skubacz <filip.skubacz00@gmail.com>
2021-12-06 17:10:52 +0100

    Remove deprecated lightspeed option

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -213,7 +213,6 @@ return require'packer'.startup({
                     grey_out_search_area = true,
                     match_only_the_start_of_same_char_seqs = true,
                     limit_ft_matches = 5,
-                    x_mode_prefix_key = '<c-x>',
                     substitute_chars = {['\r'] = '¬'},
                     instant_repeat_fwd_key = nil,
                     instant_repeat_bwd_key = nil,
